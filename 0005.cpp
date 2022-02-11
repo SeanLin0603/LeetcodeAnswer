2103c189647244ed307db76abd34be325fbba5ae
sean <sean870603tw@yahoo.com.tw>
2022-02-11 22:41:32 +0800

    Update problem 5

--- 0005.cpp
+++ 0005.cpp
@@ -2,10 +2,10 @@
 #include <string>
 using namespace std;
 
-string longestPalindrome(string s) 
+string longestPalindrome(string s)
 {
-	string result;
-	string tmpStr;
+	string longest;
+	string subString;
 
 	int stringSize = s.size();
 
@@ -18,26 +18,44 @@ string longestPalindrome(string s)
 		{
 			for (int i = startIdx; i < startIdx + length; i++)
 			{
-				tmpStr.push_back(s[i]);
+				subString.push_back(s[i]);
 			}
 			startIdx++;
-			cout << tmpStr << endl;
-	
-			
-			
-			
-			
-			tmpStr.clear();
+			//cout << subString << endl;
+
+			// check substring is Palindrome
+			int forwardIdx = 0;
+			int backwardIdx = subString.size() - 1;
+
+			bool isPalindrome = true;
+			while (backwardIdx > forwardIdx)
+			{
+				if (subString[forwardIdx] != subString[backwardIdx])
+				{
+					isPalindrome = false;
+					break;
+				}
+
+				forwardIdx++;
+				backwardIdx--;
+			}
+
+			if (isPalindrome && subString.size() > longest.size())
+			{
+				longest = subString;
+			}
+			subString.clear();
 		}
 	}
 
-
-
-	return result;
+	return longest;
 }
 
 int main()
 {
 	string input = "babad";
+	//string input = "cbbd";
+	//string input = "azwdzwmwcqzgcobeeiphemqbjtxzwkhiqpbrprocbppbxrnsxnwgikiaqutwpftbiinlnpyqstkiqzbggcsdzzjbrkfmhgtnbujzszxsycmvipjtktpebaafycngqasbbhxaeawwmkjcziybxowkaibqnndcjbsoehtamhspnidjylyisiaewmypfyiqtwlmejkpzlieolfdjnxntonnzfgcqlcfpoxcwqctalwrgwhvqvtrpwemxhirpgizjffqgntsmvzldpjfijdncexbwtxnmbnoykxshkqbounzrewkpqjxocvaufnhunsmsazgibxedtopnccriwcfzeomsrrangufkjfzipkmwfbmkarnyyrgdsooosgqlkzvorrrsaveuoxjeajvbdpgxlcrtqomliphnlehgrzgwujogxteyulphhuhwyoyvcxqatfkboahfqhjgujcaapoyqtsdqfwnijlkknuralezqmcryvkankszmzpgqutojoyzsnyfwsyeqqzrlhzbc";
+
 	cout << longestPalindrome(input);
 }
