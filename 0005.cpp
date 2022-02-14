29ff74f6bbe34e0229ccdff97cb79c63155990ba
sean <sean870603tw@yahoo.com.tw>
2022-02-14 11:16:50 +0800

    Update 5 (temp)

--- 0005.cpp
+++ 0005.cpp
@@ -9,44 +9,78 @@ string longestPalindrome(string s)
 
 	int stringSize = s.size();
 
-	for (int length = stringSize; length > 0; length--)
+	int head = 0;
+	int tail = stringSize - 1;
+
+	while (tail > head)
 	{
-		// sliding window
-		int times = stringSize - length;
-		int startIdx = 0;
-		for (int t = 0; t <= times; t++)
+		if (s[tail] != s[head])
 		{
-			for (int i = startIdx; i < startIdx + length; i++)
-			{
-				subString.push_back(s[i]);
-			}
-			startIdx++;
-			//cout << subString << endl;
-
-			// check substring is Palindrome
-			int forwardIdx = 0;
-			int backwardIdx = subString.size() - 1;
-
-			bool isPalindrome = true;
-			while (backwardIdx > forwardIdx)
-			{
-				if (subString[forwardIdx] != subString[backwardIdx])
-				{
-					isPalindrome = false;
-					break;
-				}
-
-				forwardIdx++;
-				backwardIdx--;
-			}
-
-			if (isPalindrome && subString.size() > longest.size())
-			{
-				longest = subString;
-			}
-			subString.clear();
+			tail--;
+			continue;
 		}
+
+
+		// s[tail] == s[head]
+		int forwardIdx = head;
+		int backwardIdx = tail;
+		bool isPalindrome = true;
+		while (backwardIdx > forwardIdx)
+		{
+			if (s[backwardIdx] != s[forwardIdx])
+			{
+				isPalindrome = false;
+				break;
+			}
+			forwardIdx++;
+			backwardIdx--;
+		}
+
+		head++;
 	}
+
+
+
+
+
+	//for (int length = stringSize; length > 0; length--)
+	//{
+	//	// sliding window
+	//	int times = stringSize - length;
+	//	int startIdx = 0;
+	//	for (int t = 0; t <= times; t++)
+	//	{
+	//		for (int i = startIdx; i < startIdx + length; i++)
+	//		{
+	//			subString.push_back(s[i]);
+	//		}
+	//		startIdx++;
+	//		//cout << subString << endl;
+
+	//		// check substring is Palindrome
+	//		int forwardIdx = 0;
+	//		int backwardIdx = subString.size() - 1;
+
+	//		bool isPalindrome = true;
+	//		while (backwardIdx > forwardIdx)
+	//		{
+	//			if (subString[forwardIdx] != subString[backwardIdx])
+	//			{
+	//				isPalindrome = false;
+	//				break;
+	//			}
+
+	//			forwardIdx++;
+	//			backwardIdx--;
+	//		}
+
+	//		if (isPalindrome && subString.size() > longest.size())
+	//		{
+	//			longest = subString;
+	//		}
+	//		subString.clear();
+	//	}
+	//}
 
 	return longest;
 }
